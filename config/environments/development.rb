976f6ce422461443f94a6c10c2655f17bdb7014c
faheemPakistani <mrfaheem173@gmail.com>
2022-05-23 19:23:36 +0500

    Sign up page settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:      'mrfaheem173@gmail.com',
-    password:       'mrfaheem03144315442',
+    password:       'pass',
     domain:         'localhost:3000',
     address:        'smtp.gmail.com',
     port:           '587',
